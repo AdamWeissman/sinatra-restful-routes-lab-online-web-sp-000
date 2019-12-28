fa995b481698a31ee415503b6bc74612786d6450
Adam Weissman <adam@comedytragedyepic.com>
2019-12-28 16:10:32 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20191228151928) do
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "ingredients"
+    t.string "cook_time"
+  end
+
 end
