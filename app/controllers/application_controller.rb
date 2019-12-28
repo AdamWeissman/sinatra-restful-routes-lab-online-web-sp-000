4bf2714d56ed890047113dc38f6e475573e5ff0f
Adam Weissman <adam@comedytragedyepic.com>
2019-12-28 16:04:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,25 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/recipes' do
-    
+    @recipes = Recipe.all
+    erb :index
+  end
+  
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+  
+  delete '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+  
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
 
 end
